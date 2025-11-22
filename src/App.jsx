2608094341d5ba90398839c005e363b4183fdd01
Santiago Rivera <santiagor10764@gmail.com>
2025-11-22 03:38:39 -0500

    Fixed dashboard removal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import './App.css'
 import Dashboard from "./components/dashboard";
 import Home from "./Pages/home";
@@ -8,16 +8,31 @@ import Profile from "./Pages/profile";
 import Login from "./Pages/login";
 import Register from "./Pages/register.jsx";
 
+function Layout({ children }) {
+	const location = useLocation();
+	const hideDashboard = location.pathname === "/" || location.pathname === "/register";
+
+	return (
+		<>
+			{!hideDashboard && <Dashboard />}
+			{children}
+		</>
+	);
+}
+
 function App() {
 	return (
 		<Router>
-			<Dashboard />
-			<Routes>
-				<Route path="/home" element={<Home />} />
-				<Route path="/game" element={<Game />} />
-				<Route path="/leaderboard" element={<Leaderboard />} />
-				<Route path="/profile" element={<Profile />} />
-			</Routes>
+			<Layout>
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/game" element={<Game />} />
+					<Route path="/leaderboard" element={<Leaderboard />} />
+					<Route path="/profile" element={<Profile />} />
+				</Routes>
+			</Layout>
 		</Router>
 	);
 }
