bb29a886483de961812ed60f4e223b28d14a320e
Santiago Rivera <santiagor10764@gmail.com>
2025-12-05 02:13:07 -0500

    Cleaning home design, fixed dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,18 @@ import Profile from "./Pages/profile";
 import Login from "./Pages/login";
 import Register from "./Pages/register";
 
-function Layout({ children }) {
-	const location = useLocation();
-
-	return (
-		<>
-			{!hideDashboard && <Dashboard />}
-			{children}
-		</>
-	);
-}
-
 function App() {
 	return (
 		<Router>
-			<Layout>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/game" element={<Game />} />
-					<Route path="/leaderboard" element={<Leaderboard />} />
-					<Route path="/profile" element={<Profile />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/register" element={<Register />} />
-				</Routes>
-			</Layout>
+			<Dashboard />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/game" element={<Game />} />
+				<Route path="/leaderboard" element={<Leaderboard />} />
+				<Route path="/profile" element={<Profile />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+			</Routes>
 		</Router>
 	);
 }
